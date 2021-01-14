73b91e746b5ed12214400ae79b43720e508dcc76
Radoslaw <zerek.r93@gmail.com>
2021-01-14 22:51:32 +0100

    Adding SimpleUser class in user package

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,21 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Calculator test
         Calculator simpleCalculator = new Calculator(74574, 82417);
 
         int additionResult = simpleCalculator.getAddNums();
