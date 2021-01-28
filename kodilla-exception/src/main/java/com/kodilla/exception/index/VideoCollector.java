56035ac821bce4a0ebe632ee0226589eda3f58fc
Radoslaw <zerek.r93@gmail.com>
2021-01-28 13:29:16 +0100

    8.4 Handling errors on various cases

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/index/VideoCollector.java
@@ -0,0 +1,12 @@
+package com.kodilla.exception.index;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoCollector {
+    public List<String> getCollection() {
+        ArrayList<String> viedos = new ArrayList<>();
+        viedos.add("Extra Movie");
+        return viedos;
+    }
+}
