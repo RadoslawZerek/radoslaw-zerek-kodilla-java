1d9ac37d594535093a928a098fcfb91e01b26985
Radoslaw <zerek.r93@gmail.com>
2021-03-10 22:17:10 +0100

    14.1 Added Task: bean ForumUser

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ForumUserTestSuite {
+
+    @Test
+    void testGetUserName() {
+
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser user = context.getBean(ForumUser.class);
+
+        //When
+        String userName = user.getUserName();
+
+        //Then
+        assertEquals("John Smith", userName);
+
+
+    }
+}
